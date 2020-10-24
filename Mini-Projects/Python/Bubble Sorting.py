76d0020986c2d2cf155ae8a745ff0f0d17d181dd
Sreehari Jeevan <sreeharijeevan2000@gmail.com>
2020-10-24 13:22:59 +0530

    Update Bubble Sorting.py

--- Mini-Projects/Python/Bubble Sorting.py
+++ Mini-Projects/Python/Bubble Sorting.py
@@ -3,27 +3,27 @@ Implementation of Binary Search Algorithm in Python
 '''
 
 # defining a function for bubble sort
-def bubble_sort(my_list):
+def bubble_sort(myList):
 
                 # len() to find total no of elements in list
-                length   =  len(my_list)
-                print(f"Original List : {my_list}")
+                length   =  len(myList)
+                print(f"Original List : {myList}")
 
                 # traversing through the list with for loop
                 for i in range(length - 1): 
                         for j in range(length - i - 1):
                                 #  checking and swapping values if the element is found larger
-                                if my_list[j] > my_list[j+1]:
-                                        my_list[j],  my_list[j+1] = my_list[j+1],  my_list[j]
+                                if myList[j] > myList[j+1]:
+                                        myList[j],  myList[j+1] = myList[j+1],  myList[j]
 
-                print(f"After sorting list is :  {my_list}")
+                print(f"After sorting list is :  {myList}")
 
 # creating a list through user input
-my_list = []
+myList = []
 num = int(input("Enter total no of elements =  "))
 for i in range(num):
         x = int(input("Enter no :  "))
-        my_list.append(x)
+        myList.append(x)
 
 # function call
-bubble_sort(my_list)
+bubble_sort(myList)
